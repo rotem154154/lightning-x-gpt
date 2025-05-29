3ba1d900cc229f413ebbf39d20f0c68a1b1fe5a6
Ubuntu <ubuntu@ip-172-31-44-182.eu-central-1.compute.internal>
2025-05-29 12:55:32 +0000

    removing weight tying made the training 8% slower and with 20% more params but it converge faster

--- nanogpt/model.py
+++ nanogpt/model.py
@@ -134,11 +134,9 @@ class GPT(nn.Module):
             ln_f = RMSNorm(config.n_embd, eps=1e-5, elementwise_affine=True),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
-        # with weight tying when using torch.compile() some warnings get generated:
-        # "UserWarning: functional_call was passed multiple values for tied weights.
-        # This behavior is deprecated and will be an error in future versions"
-        # not 100% sure what this is, so far seems to be harmless. TODO investigate
-        self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
+
+        # removing weight tying made the training 8% slower and with 20% more params but it converge faster
+        # self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
 
         # init all weights
         self.apply(self._init_weights)

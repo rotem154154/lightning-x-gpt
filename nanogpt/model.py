58c9b87da3e4fce0a9d12e838e2351d93f9890b0
Ubuntu <ubuntu@ip-172-31-44-182.eu-central-1.compute.internal>
2025-05-29 06:23:30 +0000

    no bias

--- nanogpt/model.py
+++ nanogpt/model.py
@@ -116,7 +116,7 @@ class GPTConfig:
     n_head: int = 12
     n_embd: int = 768
     dropout: float = 0.0
-    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    bias: bool = False # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 class GPT(nn.Module):
 

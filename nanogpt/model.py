a377c8729f40416b46df91cd7f4a22fe4bc81bec
Ubuntu <ubuntu@ip-172-31-44-182.eu-central-1.compute.internal>
2025-05-30 00:20:01 +0000

    remove simple rmsnorm

--- nanogpt/model.py
+++ nanogpt/model.py
@@ -19,7 +19,6 @@ class ReluSquared(nn.Module):
     def forward(self, x):
         return F.relu(x) ** 2
 
-<<<<<<< Updated upstream
 class Rotary(nn.Module):
     """
     Rotary position embedding applied on last dimension that
@@ -53,7 +52,7 @@ class Rotary(nn.Module):
         y1 =  x1 * cos - x2 * sin
         y2 =  x1 * sin + x2 * cos
         return torch.stack((y1, y2), dim=-1).flatten(-2)   # back to (..., D)
-=======
+
 class RotaryEmbedding(nn.Module):
     """
     Implements the original RoPE: https://arxiv.org/abs/2104.09864
@@ -101,8 +100,6 @@ class RotaryEmbedding(nn.Module):
         cos = self.cos_cached[:seq_len]  # slice to length T
         sin = self.sin_cached[:seq_len]
         return self.apply_rotary(q, cos, sin), self.apply_rotary(k, cos, sin)
-
->>>>>>> Stashed changes
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
@@ -228,17 +225,14 @@ class GPT(nn.Module):
             ln_f = RMSNorm(config.n_embd, eps=1e-5, elementwise_affine=True),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
-<<<<<<< Updated upstream
 
         # removing weight tying made the training 8% slower and with 20% more params but it converge faster
         # self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
-=======
         # with weight tying when using torch.compile() some warnings get generated:
         # "UserWarning: functional_call was passed multiple values for tied weights.
         # This behavior is deprecated and will be an error in future versions"
         # not 100% sure what this is, so far seems to be harmless. TODO investigate
         # self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying without 20% more params 10% slower
->>>>>>> Stashed changes
 
         # init all weights
         self.apply(self._init_weights)

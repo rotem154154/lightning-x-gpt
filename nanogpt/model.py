4700f520f8e5c58b990246c9bc8b54f2a8e492b3
Ubuntu <ubuntu@ip-172-31-44-182.eu-central-1.compute.internal>
2025-06-02 23:26:56 +0000

    Parallel Attention/MLP

--- nanogpt/model.py
+++ nanogpt/model.py
@@ -221,10 +221,11 @@ class Block(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
-        return x
-
+        # Parallel residual: both attn and mlp see the same input
+        attn_out = self.attn(self.ln_1(x))
+        mlp_out = self.mlp(self.ln_2(x))
+        return x + attn_out + mlp_out
+        
 @dataclass
 class GPTConfig:
     block_size: int = 1024
